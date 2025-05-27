7414f6a9b8553a8c369504c99910be7a15edfd05
horsecobbler <211329048+horsecobbler@users.noreply.github.com>
2025-05-27 12:55:52 +0100

    Split head component between app/document/home page

--- apps/front-end/src/pages/index.tsx
+++ apps/front-end/src/pages/index.tsx
@@ -12,7 +12,7 @@ import { formatNumber, truncateAddress } from "@/utils";
 const getExplorerUrl = (network: Network, txHash: string): string =>
   `https://wormholescan.io/#/tx/${txHash}?network=${network}`;
 
-export default function Home() {
+const Home = () => {
   const [balance, setBalance] = useState(0);
   const [amount, setAmount] = useState(0);
   const [gasDropoffLevel, setGasDropoffLevel] =
@@ -105,67 +105,9 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>
           Stable | Move USDC across networks with high speed and minimal costs
         </title>
-        <link rel="shortcut icon" href="./imgs/favicon-hi.png" />
-        <link rel="icon" sizes="196x196" href="./imgs/favicon-hi.png" />
-        <meta
-          itemProp="name"
-          content="Stable | Move USDC across networks with high speed and minimal costs"
-        />
-        <meta
-          itemProp="description"
-          content="Powered by Circle, Stable's SDK enables instant USDC transfers across networks."
-        />
-        <meta
-          itemProp="image"
-          content="https://stable-e58ff.web.app/imgs/og.jpg"
-        />{" "}
-        {/* @todo */}
-        <meta
-          property="og:title"
-          content="Stable | Move USDC across networks with high speed and minimal costs"
-        />
-        <meta
-          property="og:description"
-          content="Powered by Circle, Stable's SDK enables instant USDC transfers across networks."
-        />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:image"
-          content="https://stable-e58ff.web.app/imgs/og.jpg"
-        />{" "}
-        {/* @todo */}
-        <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="700" />
-        <meta
-          property="og:image:alt"
-          content="Move USDC across networks with high speed and minimal costs"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:title"
-          content="Stable | Move USDC across networks with high speed and minimal costs"
-        />
-        <meta
-          name="twitter:description"
-          content="Powered by Circle, Stable's SDK enables instant USDC transfers across networks."
-        />
-        <meta
-          name="twitter:image"
-          content="https://stable-e58ff.web.app/imgs/og.jpg"
-        />{" "}
-        {/* @todo */}
-        <meta
-          name="twitter:image:src"
-          content="https://stable-e58ff.web.app/imgs/og.jpg"
-        />{" "}
-        {/* @todo */}
       </Head>
       <div className="bridge-app main">
         <div className="backgrounds">
@@ -525,4 +467,6 @@ export default function Home() {
       </div>
     </>
   );
-}
+};
+
+export default Home;
