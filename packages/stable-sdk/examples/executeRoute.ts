a5bae4c8bb794f0a0b86010ac3e02a955cc2f942
odnods <lethalmc@gmail.com>
2025-05-30 19:03:20 -0300

    clean up examples

--- packages/stable-sdk/examples/executeRoute.ts
+++ packages/stable-sdk/examples/executeRoute.ts
@@ -3,13 +3,13 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+import dotenv from "dotenv";
+import { Address } from "viem";
+import { eth, EvmDomains } from "@stable-io/cctp-sdk-definitions";
 import { ViemSigner } from "../src/signer/viemSigner.js";
 import { privateKeyToAccount } from "viem/accounts";
-import StableSDK from "../src/index.js";
-import { Address } from "viem";
-import dotenv from "dotenv";
-import { EvmDomains } from "@stable-io/cctp-sdk-definitions";
-import { ViemEvmClient } from "@stable-io/cctp-sdk-viem";
+import StableSDK, { Route } from "../src/index.js";
+import { bigintReplacer } from "../src/utils";
 
 dotenv.config();
 const privateKey = process.env.EVM_PRIVATE_KEY as Address;
@@ -19,7 +19,7 @@ const sender = account.address;
 const recipient = account.address;
 
 const rpcUrls = {
-  Ethereum: "https://ethereum-sepolia.rpc.subquery.network/public",
+  Ethereum: "https://dimensional-solemn-scion.ethereum-sepolia.quiknode.pro/585eb5fde76eda6d2b9e4f6a150ec7bf4df12af1/",
 };
 
 const sdk = new StableSDK({
@@ -30,20 +30,17 @@ const sdk = new StableSDK({
 
 const intent = {
   sourceChain: "Ethereum" as const,
-  targetChain: "Optimism" as const,
+  targetChain: "Arbitrum" as const,
   amount: "0.01",
   sender,
   recipient,
-  gasDropoffDesired: 0n,
+  // To receive gas tokens on the target. Increases the cost of the transfer.
+  // gasDropoffDesired: eth("0.0015").toUnit("atomic"),
 };
 
 const routes = await sdk.findRoutes(intent, { paymentToken: "usdc" });
 
-console.info(`Transfers from ${intent.sourceChain} to ${intent.targetChain}.`);
-console.info(`Sender: ${sender}`);
-console.info(`Recipient: ${recipient}`);
-
-const selectedRoutes = [routes.all[3]];
+const selectedRoutes = [routes.fastest];
 
 for (const route of selectedRoutes) {
   const hasBalance = await sdk.checkHasEnoughFunds(route);
@@ -51,9 +48,37 @@ for (const route of selectedRoutes) {
     console.info(`${route.intent.sender} doesn't have enough balance to pay for the transfer`);
     continue;
   }
-  console.info("--------------------------------");
+
+  route.progress.on("step-completed", (e) => {
+    console.info(`Step completed: ${e.name}.`);
+    // console.info(`Data: ${stringify(e.data)}\n`);
+  });
+
+  route.transactionListener.on("*", (e) => {
+    console.info(`Transaction Event: ${e.name}.`);
+    // console.info(`Data: ${stringify(e.data)}\n`);
+  });
+  
+  logRouteInfo(route);
+  console.info("Executing route...");
+
+  const {
+    transactions,
+    attestations,
+    redeems,
+    transferHash,
+    redeemHash,
+  } = await sdk.executeRoute(route);
+
+  console.info(`Transfer Sent:`, getTestnetScannerTxUrl(route.intent.sourceChain, transferHash));
+  console.info(`Transfer Redeemed:`, getTestnetScannerTxUrl(route.intent.targetChain, redeemHash));
+}
+
+function logRouteInfo(route: Route) {
   console.info("");
-  console.info("");
+  console.info(`Transferring from ${intent.sourceChain} to ${intent.targetChain}.`);
+  console.info(`Sender: ${sender}`);
+  console.info(`Recipient: ${recipient}`);
   console.info(`Routing through corridor: ${route.corridor}`);
   console.info(
     "Token Authorization To use:",
@@ -71,15 +96,27 @@ for (const route of selectedRoutes) {
     console.info(`    ${fee}`);
   }
 
-  console.info("Executing route...");
-  const {
-    transactions,
-    attestations,
-    redeems,
-    transferHash,
-    redeemHash,
-  } = await sdk.executeRoute(route);
+  console.info("");
+  console.info("");
+}
 
-  console.info(`Transfer Sent:`, transferHash);
-  console.info(`Transfer Redeemed:`, redeemHash);
+function stringify(obj: any) {
+  return JSON.stringify(obj, bigintReplacer);
+}
+
+function getTestnetScannerTxUrl<D extends keyof EvmDomains>(
+  domain: D,
+  txHash: string,
+): string {
+  const scanners: Partial<Record<keyof EvmDomains, string>> = {
+    ["Ethereum"]: "https://sepolia.etherscan.io/tx/",
+    ["Arbitrum"]: "https://sepolia.arbiscan.io/tx/",
+    ["Optimism"]: "https://sepolia-optimism.etherscan.io/tx/",
+  };
+
+  const baseUrl = scanners[domain];
+
+  if (!baseUrl) return "unknown scanner address";
+
+  return `${baseUrl}${txHash}`;
 }
