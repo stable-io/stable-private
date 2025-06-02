d5f05c449868936bafc64eb954a27888a74f2fd9
horsecobbler <211329048+horsecobbler@users.noreply.github.com>
2025-06-02 03:01:14 -0700

    Fix lint in sdk

--- packages/stable-sdk/src/index.ts
+++ packages/stable-sdk/src/index.ts
@@ -2,8 +2,8 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
-export type { EvmDomains } from "@stable-io/cctp-sdk-definitions"
+export type { EvmDomains } from "@stable-io/cctp-sdk-definitions";
 
 export type * from "./types/index.js";
-export { ViemSigner } from "./signer/viemSigner.js"
+export { ViemSigner } from "./signer/viemSigner.js";
 export { StableSDK as default } from "./stable.js";
