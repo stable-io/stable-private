b44a43364ee11718381bcbd880783e14577f2ce4
horsecobbler <211329048+horsecobbler@users.noreply.github.com>
2025-06-03 06:45:39 -0700

    Add Background component

---
+++ apps/front-end/src/components/elements/Background/Background.tsx
@@ -0,0 +1,9 @@
+import type { JSX } from "react";
+
+export const Background = (): JSX.Element => (
+  <div className="backgrounds">
+    <div className="radial-right"></div>
+    <div className="radial-left"></div>
+    <div className="grid"></div>
+  </div>
+);
--- apps/front-end/src/components/elements/index.ts
+++ apps/front-end/src/components/elements/index.ts
@@ -1,2 +1,3 @@
+export * from "./Background";
 export * from "./ChainSelect";
 export * from "./WalletChip";
