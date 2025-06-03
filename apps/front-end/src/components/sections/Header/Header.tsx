0770ed98818ba1919780d4c54643aa1ae78e5b87
horsecobbler <211329048+horsecobbler@users.noreply.github.com>
2025-06-03 06:46:36 -0700

    Use router to highlight current Header item

--- apps/front-end/src/components/sections/Header/Header.tsx
+++ apps/front-end/src/components/sections/Header/Header.tsx
@@ -1,38 +1,43 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import type { JSX } from "react";
 
-export const Header = (): JSX.Element => (
-  <div test-id="eader" className="top-nav">
-    <div className="left">
-      <Link href="/">
-        <h1 className="logo"></h1>
-      </Link>
+export const Header = (): JSX.Element => {
+  const router = useRouter();
+
+  return (
+    <div test-id="header" className="top-nav">
+      <div className="left">
+        <Link href="/">
+          <h1 className="logo"></h1>
+        </Link>
+      </div>
+      <div className="right">
+        <ul className="nav">
+          <li className={router.pathname === "/bridge" ? "active" : ""}>
+            <Link href="/bridge">USDC Bridge</Link>
+          </li>
+          <li>
+            <a href="#">Stable SDK</a>
+          </li>
+          <li>
+            <a href="#">Why Stable</a>
+          </li>
+          <li>
+            <a href="mailto:hello@stable.io">Reach out</a>
+          </li>
+        </ul>
+        <button className="hamburger-menu">
+          <Image
+            src="/imgs/hamburger-menu.svg"
+            alt=""
+            unoptimized
+            height={20}
+            width={20}
+          />
+        </button>
+      </div>
     </div>
-    <div className="right">
-      <ul className="nav">
-        <li className="active">
-          <a href="#">USDC Bridge</a>
-        </li>
-        <li>
-          <a href="#">Stable SDK</a>
-        </li>
-        <li>
-          <a href="#">Why Stable</a>
-        </li>
-        <li>
-          <a href="mailto:hello@stable.io">Reach out</a>
-        </li>
-      </ul>
-      <button className="hamburger-menu">
-        <Image
-          src="/imgs/hamburger-menu.svg"
-          alt=""
-          unoptimized
-          height={20}
-          width={20}
-        />
-      </button>
-    </div>
-  </div>
-);
+  );
+};
