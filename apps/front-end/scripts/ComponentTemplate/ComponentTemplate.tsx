a2535a5298336e75c9cc916b1c5f4e94575451b5
horsecobbler <211329048+horsecobbler@users.noreply.github.com>
2025-06-05 08:55:49 -0700

    Remove component template

--- apps/front-end/scripts/ComponentTemplate/ComponentTemplate.tsx
+++
@@ -1,9 +0,0 @@
-import type { JSX } from "react";
-
-export interface ComponentTemplateProps {
-  prop: any;
-}
-
-export const ComponentTemplate = ({ prop }: ComponentTemplateProps): JSX.Element => {
-  return <div data-testid="ComponentTemplate">{prop}</div>;
-};
